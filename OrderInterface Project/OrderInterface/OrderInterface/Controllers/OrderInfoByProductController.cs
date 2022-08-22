875fb64feb033a4060f4e4250d8c58743352932f
Feride Elhennawy <40718869+FerideElhennawy@users.noreply.github.com>
2022-08-22 16:44:13 +0300

    Updated

---
+++ OrderInterface Project/OrderInterface/OrderInterface/Controllers/OrderInfoByProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrderInterface.Models;
+
+namespace OrderInterface.Controllers
+{
+    public class OrderInfoByProductController : Controller
+    {
+        MarketDatabaseEntities M = new MarketDatabaseEntities();
+        // GET: OrderInfoByProject
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OrderInterface Project/OrderInterface/OrderInterface/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrderInterface.Models;
+
+namespace OrderInterface.Controllers
+{
+    public class ProductController : Controller
+    {
+        MarketDatabaseEntities M = new MarketDatabaseEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
